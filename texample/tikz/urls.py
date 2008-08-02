17e8782efc5465b921a44180bcb76faef10d9ace
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-08-02 18:02:02 +0000

    Added settings and top level urls.

---
+++ texample/tikz/urls.py
@@ -0,0 +1,20 @@
+"""\
+URL configuration for TeXample.net's TikZ section
+
+"""
+
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    url('^$','django.views.generic.simple.direct_to_template',
+        {'template': 'tikz/tikz_index.html'},
+        name="tikz_index"),
+    
+    url(r'^resources/', include('pkgresources.urls')),
+    url(r'^builds/', include('pkgbuilds.urls')),
+    url(r'^examples/', 'dummy'),
+    url(r'^about/', 'django.views.generic.simple.direct_to_template',
+        {'template': 'tikz/tikz_about.html'},
+        name="tikz_about"),
+    
+)
