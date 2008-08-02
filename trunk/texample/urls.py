8b3335db4001c6aa5bd315252d93f7febe453c58
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-08-02 18:02:02 +0000

    Added settings and top level urls.

---
+++ trunk/texample/urls.py
@@ -0,0 +1,43 @@
+"""\
+Main URL configuration for TeXample.net
+
+"""
+
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
+    (r'^tikz/', include('texample.tikz.urls')),
+    (r'^contact/', include('contact_form.urls')),
+    (r'^builds/', include('pkgbuilds.urls')),
+    (r'^admin/(.*)', admin.site.root),    
+)
+
+### Feed configuration
+
+# TikZ section
+from pkgbuilds.feeds import LatestBuilds
+from pkgresources.feeds import LatestResources
+tikz_feed_dict = {'feed_dict' : {
+    'builds' : LatestBuilds,
+    'resources' : LatestResources,
+    'examples' : None}
+}
+
+# TODO:
+# weblog section
+# beamer section
+
+urlpatterns += patterns('',
+    url(r'^feeds/tikz/(?P<url>.*)/$', 'django.contrib.syndication.views.feed',
+        tikz_feed_dict, name = 'tikz_feeds'),
+    )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(.*)$', 'django.views.static.serve', {'document_root': 'media', 'show_indexes': True}),
+)
