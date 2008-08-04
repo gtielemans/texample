08cfe12d9b09aee99add0a43098aa8dd60d31c31
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-08-04 18:15:24 +0000

    Fixed typo in feed.

--- apps/pkgresources/feeds.py
+++ apps/pkgresources/feeds.py
@@ -6,8 +6,8 @@ current_site = Site.objects.get_current()
 from pkgresources.models import Resource
 
 class LatestResources(Feed):
-    title = "%s: Latest PGF builds" % current_site.name
+    title = "%s: Latest TikZ and PGF resources" % current_site.name
     link = "/tikz/resources/"
-    description = "sdsdsdf"
+    description = "A growing collection of links to various TikZ and PGF resources."
     def items(self):
         return Resource.objects.order_by('-pub_date')[:10]
