60e7a5c3c10fd2416178c1a4b1cc5e7f5cfde368
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-08-27 13:22:16 +0000

    Added the pyglexers package. Defines Pygment lexers for sketch and dot.

---
+++ trunk/packages/pyglexers/pyglexers/dotlexer.py
@@ -0,0 +1,52 @@
+from pygments.lexer import RegexLexer, bygroups, using
+from pygments.token import *
+from pygments.lexers.text import TexLexer
+import re
+
+class DOTLexer(RegexLexer):
+    name = 'DOT'
+    aliases = ['dot']
+    filenames = ['*.dot']
+    #flags = re.DOTALL
+    #flags = re.MULTILINE | re.DOTALL
+    tokens = {
+        'root': [
+            
+            (r'[^\S\n]+', Text),
+            #(r'//.*?\n', Comment),
+            #(r'/\*.*?\*/', Comment),
+            (r'/(\\\n)?/(\n|(.|\n)*?[^\\]\n)', Comment),
+            (r'/(\\\n)?[*](.|\n)*?[*](\\\n)?/', Comment),
+            #(r'[]{}:(),;[]', Punctuation),
+            (r'(node|edge|graph|digraph|subgraph|strict)\b',Keyword),
+            #(r'(\d+\.\d*|\d*\.\d+)([eE][+-]?[0-9]+)?', Number),
+            #(r'\d+', Number),
+            (r'.*\n', Text),
+        ],
+    }
+
+
+if __name__ == '__main__':
+    from pygments import highlight
+    from pygments.formatters import HtmlFormatter, RawTokenFormatter
+    code=r"""
+   //comment
+   /* mulitline
+   comment */
+digraph G {
+        a_1 [texlbl="$\frac{\gamma}{2x^2+y^3}$"];
+        a_1 -> a_2 -> a_3 -> a_1
+        node [texmode="math"]; //comment
+        a_1 -> b_1 -> b_2 -> a_3;
+        b_1 [label="\\frac{\\gamma}{x^2}"];
+        node [texmode="verbatim"]
+        b_4 [label="\\beta"]
+        a_3 -> b_4 -> a_1;
+    }
+"""
+    print highlight(code, DOTLexer(),HtmlFormatter())
+    #f = open('d:\pycode\experiments\sketch.html','w')
+    #highlight(code, SketchLexer(), HtmlFormatter(full=True,style='colorful'),f)
+    #f.close()
+    
+    
---
+++ trunk/packages/pyglexers/setup.py
@@ -0,0 +1,25 @@
+"""
+A small collection of lexers for use with Pygments.
+
+The package adds support for:
+
+- Sketch
+- Dot (very basic)
+
+"""
+from setuptools import setup
+
+__author__ = 'Kjell Magne Fauske'
+
+setup(
+    name='My pygment lexers',
+    version='1.0',
+    description=__doc__,
+    author=__author__,
+    packages=['pyglexers'],
+    entry_points='''
+    [pygments.lexers]
+    mysketchlexer = pyglexers:SketchLexer
+    mydotlexer = pyglexers.DotLexer
+    '''
+)
