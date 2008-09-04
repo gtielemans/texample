36257daa6a4eb7dbc5994dc83f38db4d57e0ef92
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-09-04 19:41:46 +0000

    Added texblog app.

---
+++ trunk/apps/texblog/admin.py
@@ -0,0 +1,33 @@
+
+# Admin stuff
+from django.contrib import admin
+from models import Category, Entry
+      
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('title','category','pub_date')
+    list_filter = ['category']
+
+class BlogEntryAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            "all": ("css/admin_styles.css",)
+        }
+    prepopulated_fields = {"slug": ("title",)}
+
+    fieldsets = (
+        ('Meta',{'fields': ('title','slug','pub_date','updated_date',
+                            'author','status','featured','enable_comments',
+                            ),
+            }
+        ),
+        ('Categorization',{'fields': ('categories','tags')}),
+        # Add a fixedwidth style class to the following fieldsets. Gives
+        # us a hook for styling the textarea form elements
+        ('Content',{'fields': ('markup','abstract','body'),
+            'classes' :('fixedwidth',)}),
+        ('Advanced',{'fields':('abstract_html','body_html','extra_content'),
+            'classes': ('collapse','fixedwidth')})
+    )
+
+admin.site.register(Category)
+admin.site.register(Entry,BlogEntryAdmin)
