21944ae1e713d049eb5c91c68d0a485660744509
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-09-09 20:17:13 +0000

    Added author information to examples.

--- trunk/apps/texgallery/models.py
+++ trunk/apps/texgallery/models.py
@@ -1,29 +1,44 @@
+"""Models for the TikZ and PGF examples gallery"""
 from django.db import models
 import datetime
 
 
-class Tag(models.Model):
+class CommonTagInfo(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField()
     description = models.TextField(blank=True,help_text="Optional")
 
+    def __unicode__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ("title",)
+        abstract = True
+
+
+class Tag(CommonTagInfo):
+    
+    def get_absolute_url(self):
+        return u"/pgftikzexamples/tag/%s/" % (self.slug)
+
+class Feature(CommonTagInfo):
+    """A PGF feature used by an example"""
+    
+    def get_absolute_url(self):
+        return u"/pgftikzexamples/tag/%s/" % (self.slug)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=60,blank=True)
+    last_name = models.CharField(max_length=60,blank=True)
+    url = models.URLField(verify_exists=False,blank=True)
+    email = models.EmailField(blank=True)
     
     def __unicode__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return u"/pgftikzexamples/tag/%s/" % (self.slug)
-
-class Feature(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField()
-    description = models.TextField(blank=True,help_text="Optional")
-
-    def __unicode__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return u"/pgftikzexamples/tag/%s/" % (self.slug)
+        return "%s %s" % (self.first_name, self.last_name)
+    
+    
+    
 
 
 class ExampleEntry(models.Model):
@@ -42,6 +57,10 @@ class ExampleEntry(models.Model):
     enable_comments = models.BooleanField(default=True,null=True)
     #author = models.ForeignKey(User,null=True)
     features = models.ManyToManyField(Feature, blank=True)
+    author = models.ManyToManyField(Author, blank=True)
+    
+    class Meta:
+        ordering = ("title",)
     #application = models.ManyToManyField(ExampleFeature, blank=True)
     
     #def save(self, force_insert=False, force_update=False):
