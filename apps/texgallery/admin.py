04179855c072ea4868348416a2a8ea14d5f332ad
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-09-09 19:32:37 +0000

    Started work on the tikz gallery.

---
+++ apps/texgallery/admin.py
@@ -0,0 +1,30 @@
+
+# Admin stuff
+from django.contrib import admin
+from models import ExampleEntry,Tag,Feature
+
+#try:
+#    appname = __name__.split('.')[-2]
+#except:
+#    appname = 'texgallery'
+#
+#modu = __import__('%s.models' % appname,{}, {}, [''])#, globals(), locals(), ['Example'],-1)
+#admin.site.register(getattr(modu,'Example'))
+
+class ExampleAdmin(admin.ModelAdmin):
+    list_display = ('title','created')
+    list_filter = ('features','tags')
+
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {"slug": ("title",)}
+
+      
+#class ResourceAdmin(admin.ModelAdmin):
+#    list_display = ('title','category','pub_date')
+#    list_filter = ['category']
+
+admin.site.register(Tag)
+admin.site.register(ExampleEntry,ExampleAdmin)
+admin.site.register(Feature,FeatureAdmin)
+#admin.site.register(Resource,ResourceAdmin)
