9dd3eb3063071412cba991015c2f317652efeb3d
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-10-01 06:50:27 +0000

    Added entry_count denormalization field to tags and authors. All necessary housekeeping is done when saving an ExampleEntry in the admin.

--- trunk/apps/texgallery/admin.py
+++ trunk/apps/texgallery/admin.py
@@ -15,10 +15,41 @@ class ExampleAdmin(admin.ModelAdmin):
     list_display = ('title','created')
     list_filter = ('features','tags','technical_areas','author')
     ordering = ('title',)
+    
+    def save_model(self, request, obj, form, change):
+        # Code for denormalization of entry_count. Initially I wanted to use
+        # signals to do this, but the m2m relations are updated by the admin
+        # *after* a model has been saved. It is therefore not possible to
+        # detect changes in the ExampleEntry's save method or in a signal handler.
+        
+        # Take a snapshot of the existing tags
+        dbtags = set(obj.tags.all()) | set(obj.features.all()) | \
+                set(obj.technical_areas.all()) | set(obj.author.all())
+        obj.save()
+        # Get the new set of tags
+        tags = set(form.cleaned_data['features']) | \
+               set(form.cleaned_data['tags']) | \
+               set(form.cleaned_data['technical_areas'])| \
+               set(form.cleaned_data['author'])
+        # I love sets...
+        added_tags = tags - dbtags
+        removed_tags = dbtags - tags
+        # update entry_count
+        for tag in added_tags:
+            tag.entry_count += 1
+            tag.save()
+        for tag in removed_tags:
+            tag.entry_count -= 1
+            tag.save()
+        
+
+        
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('title',)
     prepopulated_fields = {"slug": ("title",)}
+   
+    
     
 class AuthorAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("first_name","last_name")}
