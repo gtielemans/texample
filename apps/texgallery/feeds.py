0ee4e3c290e8c1c78d848a37ee98effe1beb1f22
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-10-08 06:31:37 +0000

    Added feed to texgallery.

---
+++ apps/texgallery/feeds.py
@@ -0,0 +1,17 @@
+from django.contrib.syndication.feeds import Feed
+from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse
+
+current_site = Site.objects.get_current()
+
+from texgallery.models import ExampleEntry
+
+class LatestExamples(Feed):
+    title = "Latest additions to the TikZ and PGF examples gallery" 
+    link = reverse('texgallery_index')
+    description = ""
+    def items(self):
+        return ExampleEntry.live.all()[:10]
+    
+    def item_pubdate(self, item):
+        return item.created
