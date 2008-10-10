ec24d5976ef20ca8850d1078071c326238d3b652
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2008-10-10 17:53:33 +0000

    Changed texblog to allow viewing draft entries from admin.

--- trunk/apps/texblog/urls.py
+++ trunk/apps/texblog/urls.py
@@ -13,6 +13,12 @@ entry_info_dict = {
     'date_field': 'pub_date',
     }
 
+entry_info_dict_all = {
+    'queryset': Entry.objects.all(),
+    'date_field': 'pub_date',
+    }
+
+
 #
 #urlpatterns = patterns('',
 #    url('^$','django.views.generic.list_detail.object_list',entries_dict,name='texblog_index'),
@@ -36,6 +42,6 @@ urlpatterns = patterns('',
         name='texblog_entry_archive_month'),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
         date_based.object_detail,
-        dict(entry_info_dict, template_object_name = 'entry',slug_field='slug'),
+        dict(entry_info_dict_all, template_object_name = 'entry',slug_field='slug'),
         name='texblog_entry_detail'),
 )
