d5c3833ddd80779a19f08c2494b651eeaa56e774
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2009-05-27 18:11:36 +0000

    Fixed error in setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='texamplenet',
       version='1.0dev',
-      package_dir={'texample':'texample','':'apps'},
+      package_dir={'texample':'texample/','':'apps'},
       packages=[
         'texample','texgallery','texarticles','texblog','texpub',\
         'texpubutils','ganalytics','pkgbuilds','pkgresources',\
