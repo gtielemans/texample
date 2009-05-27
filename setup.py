5b873f96b57ca36ad5c95d85123e1cedd5727043
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2009-05-27 17:49:51 +0000

    Added a setup file for easier installation using PIP.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='texamplenet',
+      version='1.0dev',
+      package_dir={'texample':'texample','':'apps'},
+      packages=[
+        'texample','texgallery','texarticles','texblog','texpub',\
+        'texpubutils','ganalytics','pkgbuilds','pkgresources',\
+        ],
+      )
