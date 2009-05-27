2c255dcfc26ae632f93ec62e7ac081b1254e6203
kjellmf <kjellmf@66a8ba94-7553-0410-a5b9-597b14aa6dd6>
2009-05-27 18:48:26 +0000

    New setup.py attempt.

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+#from distutils.core import setup
+
+from setuptools import setup, find_packages
 
 setup(name='texamplenet',
       version='1.0dev',
-      package_dir={'texample':'texample/','':'apps'},
-      packages=[
-        'texample','texgallery','texarticles','texblog','texpub',\
-        'texpubutils','ganalytics','pkgbuilds','pkgresources',\
-        ],
+      #package_dir={'texample':'texample/','':'apps'},
+      packages=find_packages(),#[
+      #  'texample','texgallery','texarticles','texblog','texpub',\
+      #  'texpubutils','ganalytics','pkgbuilds','pkgresources',\
+      #  ],
       )
